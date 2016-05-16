fd7e03f4bf35927a129437df9c201161db2aaf61
ITManager <zhangjiesharp@163.com>
2016-05-16 17:17:43 +0800

    01.实现了开始刷新功能

---
+++ CZRefresh29/CZRefresh29/Refresh/CZRefresh.h
@@ -0,0 +1,15 @@
+//
+//  CZRefresh.h
+//  CZRefresh29
+//
+//  Created by zhangjie on 16/5/16.
+//  Copyright © 2016年 zhangjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CZRefresh : UIControl
+
+- (void)startRefresh;
+
+@end
---
+++ CZRefresh29/CZRefresh29/Refresh/CZRefresh.m
@@ -0,0 +1,18 @@
+//
+//  CZRefresh.m
+//  CZRefresh29
+//
+//  Created by zhangjie on 16/5/16.
+//  Copyright © 2016年 zhangjie. All rights reserved.
+//
+
+#import "CZRefresh.h"
+
+@implementation CZRefresh
+
+- (void)startRefresh
+{
+    NSLog(@"开始刷新");
+}
+
+@end
